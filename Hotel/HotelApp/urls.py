ccecacac995b3d1875a37242350567a9b8855438
Bayan Bayrakdar <bayan.bayrakdar98@gmail.com>
2025-02-09 10:33:27 +0200

    Room Mangament

--- Hotel/HotelApp/urls.py
+++ Hotel/HotelApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.RoomList.as_view()),
-    path('manageRoom/',views.manageRoom.as_view()),
+    path('',views.HotelApp),
+    path('RoomAll/',views.RoomAll),
+    path('RoomUpdate/<int:id>',views.update_room),
+    path('RoomDelete/<int:id>',views.delete_room),
+    path('RoomAdd/',views.add_room),
+    path('RoomFilter/',views.RoomFilter.as_view()),
     path('RoomSearch/',views.RoomSearch.as_view()),
 ]
--- Hotel/HotelApp/views.py
+++ Hotel/HotelApp/views.py
@@ -1,24 +1,83 @@
 from calendar import c
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 from .models import *
 from .serializers import *
 # Create your views here.
 from rest_framework import generics,filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import api_view
 
-class RoomList(generics.ListAPIView):
+
+@api_view(['GET'])
+def HotelApp(request):
+    api_urls = {
+        'welcome message':'/hotelapp',
+        'all_Room': '/RoomAll',
+        'Add': '/RoomAdd',
+        'Update': '/RoomUpdate/pk',
+        'Delete': '/RoomDelete/pk',
+        'Filter': '/RoomFilter',
+        'Search': '/RoomSearch',
+    }
+    return Response(api_urls)
+####### Room Management   ##########
+
+
+# get all rooms
+@api_view(['GET'])
+def RoomAll(request):
+    room=Room.objects.all()
+    if room.exists():
+
+        serializer=RoomSerializer(room,many=True)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+# update room
+@api_view(['PATCH'])
+def update_room(request,id):
+    room=request.get(id=id)
+    data=RoomSerializer(instance=room,data=request.data)
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+# add room
+@api_view(['POST'])
+def add_room(request):
+    item=RoomSerializer(data=request.data,context={'request': request})
+    if item.is_valid():
+        item.save()
+        return Response(item.data)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+# delete room
+@api_view(['DELETE'])
+def delete_room(request, id):
+    item = Room.objects.get(id=id)
+    item.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+# filter room
+class RoomFilter(generics.ListAPIView):
     queryset=Room.objects.all()
     serializer_class=RoomSerializer
     filter_backends=[DjangoFilterBackend]
     filterset_fields=['Room_num','Room_type','Price','Room_status']
-    search_fields=['Room_num','Room_type','Price','Room_status']
-
+    
+# search room
 class RoomSearch(generics.ListAPIView):
     queryset=Room.objects.all()
     serializer_class=RoomSerializer
     filter_backends=[filters.SearchFilter]
     search_fields=['Room_num','Room_type','Price','Room_status']
 
-class manageRoom(generics.ListAPIView):
-    queryset=Room.objects.all()
-    serializer_class=RoomSerializer
+
+
+###### Booking Management #########
